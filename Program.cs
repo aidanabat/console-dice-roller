7819c7de8aa0df21239071388b1be7e8ef312664
aidanabat <aidanabat@gmail.com>
2020-08-15 20:02:23 +0800

    First version of program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dice_Roller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
